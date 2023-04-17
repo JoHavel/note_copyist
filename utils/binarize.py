480101a6796415d214e7f8411661fe6f95860686
Jonáš Havelka <jonas.havelka@volny.cz>
2023-04-17 09:19:59 +0200

    Add: probability binarizer

--- utils/binarize.py
+++ utils/binarize.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import tensorflow_probability as tfp
+
 
 class Binarize(tf.keras.Model):
     def __init__(self, model, threshold):
@@ -8,3 +10,12 @@ class Binarize(tf.keras.Model):
 
     def __call__(self, *args, **kwargs):
         return tf.where(self.model(*args, **kwargs) > self.threshold, 1.0, 0.0)
+
+
+class ProbBinarize(tf.keras.Model):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def __call__(self, *args, **kwargs):
+        return tfp.distributions.Bernoulli(probs=self.model(*args, **kwargs)).sample()
