850c0254ccfd552f86eb0c88fd259ca7788bdaca
Jonáš Havelka <jonas.havelka@volny.cz>
2023-05-13 14:18:51 +0200

    Feat: rewritten discriminator as encoder to one dimension

--- models/discriminator.py
+++ models/discriminator.py
@@ -1,33 +1,18 @@
 import tensorflow as tf
 
+from encoder import _body
+
 
 def discriminator(
         input_shape: list[int],
-        layers: list[int] = [128],
-        conv_layers: list[int] = [],
+        hidden_layers: list[int] = (128,),
+        conv_layers: list[int] = (),
         kernel_size: int = 5,
         stride: int = 2,
         optimizer: tf.keras.optimizers.Optimizer = None,
 ) -> tf.keras.Model:
-    if len(conv_layers) != 0 and len(input_shape) < 2:
-        raise ValueError("Conv for 1D or scalar data!")
-
-    inp = tf.keras.layers.Input(input_shape)
-    last_layer = inp
-
-    if len(input_shape) > 1:
-        if len(conv_layers) > 0 and len(input_shape) == 2:
-            last_layer = last_layer[..., None]
-
-        for cl in conv_layers:
-            last_layer = tf.keras.layers.Conv2D(cl, kernel_size=kernel_size, strides=stride, use_bias=False)(last_layer)
-            last_layer = tf.keras.layers.BatchNormalization()(last_layer)
-            last_layer = tf.keras.layers.Activation(activation="relu")(last_layer)
-
-        last_layer = tf.keras.layers.Flatten()(last_layer)
-
-    for layer in layers:
-        last_layer = tf.keras.layers.Dense(layer, activation="relu")(last_layer)
+    """ Create neural network, that encodes data to True (1) or False (0). """
+    inp, last_layer = _body(input_shape, [1], hidden_layers, conv_layers, kernel_size, stride)
 
     last_layer = tf.keras.layers.Dense(1, activation="sigmoid")(last_layer)[0]
 
