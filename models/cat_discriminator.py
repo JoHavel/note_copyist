adf314de67e1ce05beb325ef670d4bd303127c68
Jonáš Havelka <jonas.havelka@volny.cz>
2023-05-13 16:51:06 +0200

    Feat: categorical discriminator assembled from parts

--- models/cat_discriminator.py
+++ models/cat_discriminator.py
@@ -1,45 +1,47 @@
 import tensorflow as tf
 
+from .encoder import _body_checks, _conv_downsample, _fully_connected
+from .discriminator import _discriminator_head
 
-def discriminator(
+
+def _body(
         input_shape: tuple[list[int], int],
-        layers: list[int] = [128],
-        conv_layers: list[int] = [],
-        kernel_size: int = 5,
-        stride: int = 2,
-        optimizer: tf.keras.optimizers.Optimizer = None,
-) -> tf.keras.Model:
-    if len(conv_layers) != 0 and len(input_shape[0]) < 2:
-        raise ValueError("Conv for 1D or scalar data!")
+        hidden_layers: list[int],
+        conv_layers: list[int],
+        kernel_size: int,
+        stride: int
+):  # -> (input, last_layer)
+    """ Create the processing part of neural network (input, conv layers, hidden layers) """
+    _body_checks(input_shape[0], conv_layers)
 
+    # INPUT
     inp = tf.keras.layers.Input(input_shape[0])
     last_layer = inp
 
+    # LAYERS
+    if len(conv_layers) > 0:
+        last_layer = _conv_downsample(last_layer, conv_layers, kernel_size, stride, len(input_shape[0]) == 2)
+
     if len(input_shape[0]) > 1:
-        if len(conv_layers) > 0 and len(input_shape[0]) == 2:
-            last_layer = last_layer[..., None]
-
-        for cl in conv_layers:
-            last_layer = tf.keras.layers.Conv2D(cl, kernel_size=kernel_size, strides=stride, use_bias=False)(last_layer)
-            last_layer = tf.keras.layers.BatchNormalization()(last_layer)
-            last_layer = tf.keras.layers.Activation(activation="relu")(last_layer)
-
         last_layer = tf.keras.layers.Flatten()(last_layer)
 
     cat_inp = tf.keras.layers.Input([input_shape[1]])
     last_layer = tf.keras.layers.Concatenate()([last_layer, cat_inp])
 
-    for layer in layers:
-        last_layer = tf.keras.layers.Dense(layer, activation="relu")(last_layer)
+    if len(hidden_layers) > 0:
+        last_layer = _fully_connected(last_layer, hidden_layers)
 
-    last_layer = tf.keras.layers.Dense(1, activation="sigmoid")(last_layer)[0]
+    return [inp, cat_inp], last_layer
 
-    model = tf.keras.Model(inputs=[inp, cat_inp], outputs=last_layer, name="Discriminator")
 
-    model.compile(
-        optimizer=optimizer if optimizer is not None else tf.keras.optimizers.Adam(),
-        loss=tf.losses.BinaryCrossentropy(),
-        metrics=tf.metrics.BinaryAccuracy(),
-    )
-
-    return model
+def discriminator(
+        input_shape: tuple[list[int], int],
+        hidden_layers: list[int] = (128,),
+        conv_layers: list[int] = (),
+        kernel_size: int = 5,
+        stride: int = 2,
+        optimizer: tf.keras.optimizers.Optimizer = None,
+) -> tf.keras.Model:
+    """ Create neural network, that encodes labeled data to True (1) or False (0). """
+    inp, last_layer = _body(input_shape, hidden_layers, conv_layers, kernel_size, stride)
+    return _discriminator_head(inp, last_layer, optimizer, "Discriminator_with_categorical_input")
