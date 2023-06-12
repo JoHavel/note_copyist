406a28a2cce2eb707b195d975ec93e4d3532a552
Jonáš Havelka <jonas.havelka@volny.cz>
2023-06-12 21:57:49 +0200

    Add: padding Mnist to multiply of …

--- datasets/mnist.py
+++ datasets/mnist.py
@@ -3,10 +3,21 @@ from keras.datasets import mnist
 
 from datasets.dataset import CategoricalDataset, DatasetPart
 
+_WIDTH = 28
 
 class MnistDataset(CategoricalDataset):
-    def __init__(self, train_len: int = 50000):
+    def __init__(self, train_len: int = 50000, multiply_of: int | None = None):
         (X_train, y_train), (X_test, y_test) = mnist.load_data()
+        target_width = _WIDTH
+        if multiply_of is not None:
+            target_width += multiply_of - (_WIDTH % multiply_of)
+
+            X_train = tf.image.pad_to_bounding_box(
+                X_train[..., None], (target_width - _WIDTH)//2, (target_width - _WIDTH)//2, target_width, target_width
+            )[..., 0]
+            X_test = tf.image.pad_to_bounding_box(
+                X_test[..., None], (target_width - _WIDTH)//2, (target_width - _WIDTH)//2, target_width, target_width
+            )[..., 0]
         X_train = tf.cast(X_train, tf.float32) / 255.0
         X_test = tf.cast(X_test, tf.float32) / 255.0
 
@@ -18,5 +29,5 @@ class MnistDataset(CategoricalDataset):
 
         X = {DatasetPart.TRAIN: X_train, DatasetPart.VAL: X_val, DatasetPart.TEST: X_test}
         y = {DatasetPart.TRAIN: y_train, DatasetPart.VAL: y_val, DatasetPart.TEST: y_test}
-        super().__init__((28, 28), X, y, "mnist", 10)
+        super().__init__((target_width, target_width), X, y, "mnist", 10)
 
