83d9e224cfaa23c2c1d7f59bc67a679cff31f6dd
Jonáš Havelka <jonas.havelka@volny.cz>
2023-07-03 11:31:21 +0200

    Remove: old experiments

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -1,13 +1,1 @@
 """ Particular experiments with ../generators """
-
-SEED = 42
-THREADS = 3
-
-import tensorflow as tf
-import os
-
-# From npfl114
-os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")  # Report only TF errors by default
-tf.keras.utils.set_random_seed(SEED)
-tf.config.threading.set_inter_op_parallelism_threads(THREADS)
-tf.config.threading.set_intra_op_parallelism_threads(THREADS)
