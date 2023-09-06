24ce5d36c49341388037edcbd37b6229aa4f8eaa
Jonáš Havelka <jonas.havelka@volny.cz>
2023-09-06 23:00:09 +0200

    Fix: python -> python3.11

--- latent_prepare.sh
+++ latent_prepare.sh
@@ -4,4 +4,4 @@ mv downloaded/muscima-exported-symbols/eight-note-down downloaded/muscima-export
 mv downloaded/muscima-exported-symbols/eight-note-up downloaded/muscima-exported-symbols/eighth-note-up || :
 
 SEED=$1
-python padd_muscima.py downloaded/MUSCIMA$SEED MUSCIMA --seed $SEED
+python3.11 padd_muscima.py downloaded/MUSCIMA$SEED MUSCIMA --seed $SEED
