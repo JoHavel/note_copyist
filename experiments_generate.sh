d84d4d88bf825e87da166e96fe9accb11e7e6096
Jonáš Havelka <jonas.havelka@volny.cz>
2023-09-09 00:37:30 +0200

    Add: mashcima experiments

---
+++ experiments_generate.sh
@@ -0,0 +1,16 @@
+. lib.sh
+
+# Arguments
+SEED=$1
+EXPERIMENT=$2
+
+# Body
+
+# comment for usage without running latent part
+if [ "$EXPERIMENT" == "E" ]; then
+  rm -r "$(EXPERIMENT_OUTPUT "$SEED" E)"
+  cp -r "$(LATENT_OUTPUT "$SEED" "$FINAL_LATENT")" "$(EXPERIMENT_OUTPUT "$SEED" E)"
+  exit 0
+fi
+
+"generate_${EXPERIMENT}" "$SEED"
---
+++ experiments_train.sh
@@ -0,0 +1,8 @@
+. lib.sh
+
+# Arguments
+SEED=$1
+DATASET=$2
+
+# Body
+"train_${DATASET}" "$SEED"
