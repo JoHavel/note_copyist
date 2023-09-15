cbe62b17ee82b74defa6186f30b31de52f0fe9f4
mayer <jirkamayer97@gmail.com>
2023-09-15 12:06:23 +0200

    Generation scripts for slurm

--- slurm-experiments-generate.sh
+++ slurm-experiments-generate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -J latentSymGen
-#SBATCH --array=72,73,74
-#SBATCH --output=out/logs/experiments-%A_%a.out
-#SBATCH --error=out/logs/experiments-%A_%a.err
+#SBATCH --array=0-7
+#SBATCH --output=out/logs/generate-%A_%a.out
+#SBATCH --error=out/logs/generate-%A_%a.err
 #SBATCH -p gpu-ms
 #SBATCH --gpus=1
 #SBATCH --mem=32gb
@@ -10,24 +10,35 @@
 # exclude to not take up Titans I'll need for the evaluation
 
 # you do:
-# sbatch ./slurm-experiments.sh REBELO|MIX
+# sbatch ./slurm-experiments-generate.sh 30
 
-MODEL_KIND=$1
-SEED=$SLURM_ARRAY_TASK_ID
+ID_TO_EXPERIMENT="
+A
+B
+C
+D
+E
+F
+G
+H
+"
+
+EXPERIMENT=$(echo "$ID_TO_EXPERIMENT" | head -n $(expr 2 + $SLURM_ARRAY_TASK_ID) | tail -n 1)
+SEED=$1
 
 if [ -z "$SEED" ]; then
-    echo "Model kind argument missing"
+    echo "Seed argument missing"
     exit 1
 fi
 
 echo "################################"
-echo "# Latent symbols kind $MODEL_KIND, seed $SEED"
+echo "# Generating symbols experiment $EXPERIMENT, seed $SEED"
 echo "################################"
 echo
 
 export LD_LIBRARY_PATH=/opt/cuda/11.8/lib64:/opt/cuda/11.8/cudnn/8.6.0/lib64
 
-stdbuf -o0 -e0 bash ./experiments_train.sh $SEED $MODEL_KIND
+stdbuf -o0 -e0 bash ./experiments_generate.sh $SEED $EXPERIMENT
 
 echo
 echo "########"
